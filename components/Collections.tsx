2dde2d34e5003462a8bb943c86d5ca1b0e26673d
usama387 <128306049+usama387@users.noreply.github.com>
2024-04-04 14:11:22 +0500

    Added ternary operator in collections

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -5,26 +5,30 @@ import React from "react";
 
 const Collections = async () => {
   // first function in fetcher.ts to fetch collections
-  const Collections = await getCollections();
+  const collections = await getCollections();
 
-  //   the collections are being mapped to display Image, title and products
-
+  //the collections are being mapped with some conditions to display Image, title and products
   return (
     <div className="flex flex-col items-center gap-10 py-8 px-5">
-      <p className="text-heading1-bold text-[#acabb4]">Collections</p>
-      <div className="flex items-center justify-center gap-8">
-        {Collections.map((collection: CollectionType) => (
-          <Link href={`/collections${collection._id}`} key={collection._id}>
-            <Image
-              src={collection.image}
-              alt={collection.title}
-              width={300}
-              height={160}
-              className="rounded-lg cursor-pointer"
-            />
-          </Link>
-        ))}
-      </div>
+      <p className="text-heading1-bold">Collections</p>
+      {!collections || collections.length === 0 ? (
+        <p className="text-body-bold">No collections found</p>
+      ) : (
+        <div className="flex flex-wrap items-center justify-center gap-8">
+          {collections.map((collection: CollectionType) => (
+            <Link href={`/collections/${collection._id}`} key={collection._id}>
+              <Image
+                key={collection._id}
+                src={collection.image}
+                alt={collection.title}
+                width={350}
+                height={200}
+                className="rounded-lg cursor-pointer"
+              />
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
